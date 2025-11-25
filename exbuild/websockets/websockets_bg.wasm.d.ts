487345fd3dc3edf212b08cb08b2f0c1d40fa40b9
Spxg <unsafe@outlook.es>
2025-11-25 00:33:57 +0000

    Deploying to gh-pages from @ wasm-bindgen/wasm-bindgen@f61a588f674304964a2062b2307edb304aed4d16 🚀

---
+++ exbuild/websockets/websockets_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start_websocket: () => void;
+export const wasm_bindgen__convert__closures_____invoke__h11cf8536bf1e6b88: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__closure__destroy__h0fe769e66d7ae79e: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0871a297abfc0c5c: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
